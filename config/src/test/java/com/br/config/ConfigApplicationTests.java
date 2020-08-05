8993ee844e268f3364075c637417145f151f9cf7
Neto Deolino <netodeolino@gmail.com>
2020-08-05 15:04:37 -0300

    the end

---
+++ config/src/test/java/com/br/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
